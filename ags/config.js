1be6a92d9994e4816547c759266a79e9a12b6b81
Denis Ezhov <ezhoden@mail.ru>
2024-07-21 01:33:47 +0400

    ags test modules, ags as app launcher, hyprland multifile config

--- ags/config.js
+++ ags/config.js
@@ -1,3 +1,6 @@
+import { applauncher } from './modules/applauncher.js'
+import { NotificationPopups } from './modules/notifications.js'
+
 const myLabel = Widget.Label({
 	label: 'ezhoden'
 })
@@ -8,8 +11,21 @@ const myBar = Widget.Window({
 	child: myLabel,
 })
 
+Utils.timeout(100, () => Utils.notify({
+    summary: "Notification Popup Example",
+    iconName: "info-symbolic",
+    body: "Lorem ipsum dolor sit amet, qui minim labore adipisicing "
+        + "minim sint cillum sint consectetur cupidatat.",
+    actions: {
+        "Cool": () => print("pressed Cool"),
+    },
+}))
+
 App.config({
+	style: App.configDir + "/style.css",
 	windows: [
 		myBar,
+		applauncher,
+		NotificationPopups(),
 	]
 })
