5e7189fa23dab1a4447bede3011696a3703627d0
Denis Ezhov <ezhoden@mail.ru>
2024-07-21 03:35:02 +0400

    hypr improvements, ags separate style files (app launcher doesn't work tho)

--- ags/config.js
+++ ags/config.js
@@ -1,31 +1,12 @@
-import { applauncher } from './modules/applauncher.js'
-import { NotificationPopups } from './modules/notifications.js'
-
-const myLabel = Widget.Label({
-	label: 'ezhoden'
-})
-
-const myBar = Widget.Window({
-	name: 'bar',
-	anchor: ['top', 'left', 'right'],
-	child: myLabel,
-})
-
-Utils.timeout(100, () => Utils.notify({
-    summary: "Notification Popup Example",
-    iconName: "info-symbolic",
-    body: "Lorem ipsum dolor sit amet, qui minim labore adipisicing "
-        + "minim sint cillum sint consectetur cupidatat.",
-    actions: {
-        "Cool": () => print("pressed Cool"),
-    },
-}))
+import { applauncher } from './modules/applauncher/applauncher.js'
+//import { Bar } from './modules/bar/bar.js'
+//import { NotificationPopups } from './modules/notifications/notifications.js'
 
 App.config({
-	style: App.configDir + "/style.css",
+	style: "./styles.css",
 	windows: [
-		myBar,
 		applauncher,
-		NotificationPopups(),
+		//Bar(),
+		//NotificationPopups(),
 	]
 })
