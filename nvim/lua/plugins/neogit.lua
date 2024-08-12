c5ef5b1ead1349c007f686ae9768f845d34f76d6
Denis Ezhov <ezhoden@mail.ru>
2024-08-12 17:06:44 +0400

    add some more hotkeys for neogit

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -15,3 +15,7 @@ neogit.setup({
 
 vim.keymap.set("n", '<leader>gg', neogit.open)
 vim.keymap.set("n", '<leader>gc', ':Neogit commit<CR>')
+vim.keymap.set("n", "<leader>gp", ":Neogit pull<CR>")
+vim.keymap.set("n", "<leader>gP", ":Neogit push<CR>")
+vim.keymap.set("n", "<leader>gb", ":Telescope git_branches<CR>")
+vim.keymap.set("n", "<leader>gB", ":G blame<CR>")
